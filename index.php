abf5475fb3bbcc2fab20f1573d0132e148f0db89
kurvin <kurvin@klarity.co.za>
2017-05-02 11:15:04 +0200

    Quick updates for cleaner code execution seperating cc and recipient

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ $body = '<html><body>I wrestled a bear once</body>
         <img src="https://www.thrifty.co.za/images/logo.jpg">
         </html>';
 
-$cc = 'kurvinhendricks@gmail.com,kurvintwitter@gmail.com';
-
+$recipient = 'kurvinhendricks@gmail.com';
+$cc = 'kurvindroid@gmail.com,kurvintwitter@gmail.com';
 
 //setting recipients to cc for now, to allow for mulitple recipients.
 
-$status = $mailer->SendEmailToClient($cc, 'thisisatestmailerhere', $body, $cc);
+$status = $mailer->SendEmailToClient($recipient, 'thisisatestmailerhere', $body, $cc);
 
 if($status === 0) {
     echo 'Good to go';
